d5eefb2d30e3bb29ee122b7858187a0150541733
Rene Buhrs <rbuhrs@gmail.com>
2023-06-07 18:05:16 +0200

    test: add test directly updating the nested object props

--- tests/issues/GH_new_issue.test.ts
+++ tests/issues/GH_new_issue.test.ts
@@ -151,3 +151,99 @@ test(`GH issue updating nested props`, async () => {
 
   expect(finalRecipe).toMatchObject(updatedRecipe);
 });
+
+test(`GH issue updating nested props directly`, async () => {
+  const e = orm.em.create(Recipe, {
+    id: 1,
+    name: 'Pizza',
+    instructions: {
+      ingredients: [
+        {
+          name: 'Tomato',
+          quantity: {
+            units: 1,
+            uom: 'pcs',
+          },
+        },
+        {
+          name: 'Salami',
+          quantity: {
+            units: 2,
+            uom: 'pcs',
+          },
+        },
+        {
+          name: 'Cheese',
+          quantity: {
+            units: 1,
+            uom: 'pcs',
+          },
+        },
+      ],
+      cooking: {
+        Oven : {
+          degrees: 200,
+          time: 12,
+        },
+        Microwave: {
+          degrees: 180,
+          time: 15,
+        },
+      },
+      notes: 'do not cook it too long',
+    },
+  });
+  await orm.em.persistAndFlush(e);
+
+  const e1 = await orm.em.findOneOrFail(Recipe, 1);
+  const updatedRecipe: Recipe = {
+    id: 1,
+    name: 'Pizza',
+    instructions: {
+      ingredients: [
+        {
+          name: 'Tomato',
+          quantity: {
+            units: 1,
+            uom: 'pcs',
+          },
+        },
+        {
+          name: 'Salami',
+          quantity: {
+            units: 2,
+            uom: 'pcs',
+          },
+        },
+        {
+          name: 'Cheese',
+          quantity: {
+            units: 100,
+            uom: 'gram',
+          },
+        },
+      ],
+      cooking: {
+        Oven : {
+          degrees: 200,
+          time: 12,
+        },
+        // when passing undefined object the test will fail, the cooking object will not be updated
+        // only omitting the object or passing null works
+        // Microwave: undefined,
+      },
+    },
+  };
+  e1.instructions.ingredients[2].quantity = {
+    units: 100,
+    uom: 'gram',
+  };
+  e1.instructions.notes = undefined;
+
+  await orm.em.flush();
+
+  const reloadedRecipe = await orm.em.fork().findOneOrFail(Recipe, 1);
+  const finalRecipe = wrap(reloadedRecipe).toObject();
+
+  expect(finalRecipe).toMatchObject(updatedRecipe);
+});
